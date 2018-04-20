52ed249428bd605c80a9be99e13f1a6864677a69
zunath <coolty3001@yahoo.com>
2018-04-20 10:12:03 +0800

    Fix swapping between skills.

--- Freescape.Web/ClientApp/components/Skills.jsx
+++ Freescape.Web/ClientApp/components/Skills.jsx
@@ -35,7 +35,7 @@ export default class Skills extends React.Component {
         const name = target.name;
 
         if (value === undefined)
-            value = target.skillid;
+            value = target.getAttribute('data-skillid');
         
         this.setState({
             [name]: value
